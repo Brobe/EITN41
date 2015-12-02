fb30ce1bc84279217b85f0f07d56494fcf63d7f9
Johan Nyholm <hansjohannyholm@gmail.com>
2015-12-02 15:46:56 +0100

    cleanup

--- A3/c1.py
+++ A3/c1.py
@@ -5,22 +5,21 @@ import random
 import sys
 import numpy as np
 from collections import defaultdict
-
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 
+K_SIZE = 16
 
-FAILURE = -1
-
-class Enemy(object):
+class SteveTheEnemy(object):
+	'''
+	Steve, the misguided genius who wants to
+	break your schemes and read your votes.
+	'''
 	def __init__(self, hash_func):
-		self.hash_f = hash_func
+		self.hash = hash_func
 		self.hash_to_vote = defaultdict(lambda :defaultdict(int))
-		for k in xrange((1 << 16)):
-			zero_hash = self.hash_f(0, k)
-			one_hash = self.hash_f(1, k)
+		for k in xrange((1 << K_SIZE)):
+			zero_hash = self.hash(0, k)
+			one_hash = self.hash(1, k)
 			self.hash_to_vote[zero_hash][0] += 1
 			self.hash_to_vote[one_hash][1] += 1
 
@@ -33,61 +32,61 @@ class Enemy(object):
 
 	def concealing_breakable(self, ballot):
 		'''
-		Find out if it is possible to single out the hash
-		so that it only votes os a special kind can generate
-		the ballot
+		find out if we can find k0 and k1 such that
+		h(0, k0) == h(1, k1) == ballot.
+		If so, we cannot break the concealing property
+		Returns whether or not we can break the concealing property
 		'''
 		zero_count = self.hash_to_vote[ballot][0]
 		one_count = self.hash_to_vote[ballot][1]
 		return not (zero_count > 0 and one_count > 0)
 
-class Hash(object):
+
+class XBitHash(object):
 	def __init__(self, X):
 		self.X = X
 
-	def hash_f(self, v, k):
+	def hash(self, v, k):
 		dig = hashlib.sha256(str(v) + str(k)).hexdigest()
 		return int(dig, 16) & (1 << self.X) - 1
-
-def rand_16():
-	mask_16 = (1<<16) - 1
-	return random.randint(0, sys.maxint) & mask_16
-
-def probability(zero_count, one_count):
-	'''
-	Return the probability of guessing correctly
-	'''
-	if zero_count > 0 and one_count > 0:
-		return 0
-	else:
-		return 1
 
 
 def simulate_break(X, v, n_itter):
 	'''
 	Returns:
-		cost, success_rate
-	 		cost: the average number of tries for the successes,
-	 		success_rate: The successrate
+		binding_break_probability, concealing_break_probability
 	'''
-	hash_func = Hash(X).hash_f
-	enemy = Enemy(hash_func)
+	hash_func = XBitHash(X).hash
+	enemy = SteveTheEnemy(hash_func)
 	n_binding_breaks = 0.0
 	n_concealing_breaks = 0.0
 	for _ in xrange(n_itter):
-		k = rand_16()
+		k = random.getrandbits(K_SIZE)
 		original_ballot = hash_func(v, k)
 		binding_broken = enemy.binding_breakable(original_ballot, v)
 		concealing_broken = enemy.concealing_breakable(original_ballot)
 		n_binding_breaks += binding_broken
 		n_concealing_breaks += concealing_broken
 
-	print "n_binding_breaks/n_itter", n_binding_breaks/n_itter, "n_binding_breaks",\
-		n_binding_breaks, "n_itter", n_itter
-
-	print "n_concealing_breaks/n_itter", n_concealing_breaks/n_itter, "n_concealing_breaks",\
-		n_concealing_breaks, "n_itter", n_itter
 	return n_binding_breaks/n_itter, n_concealing_breaks/n_itter
+
+def plot1(XS, probability, Y_label="probability", title="probability"):
+	plt.plot(XS, probability, linewidth=2.0)
+	plt.xlabel('X, number of bits in hash outputs')
+	plt.ylabel(Y_label)
+	plt.legend([Y_label], loc='upper left')
+	plt.title(title)
+	plt.show()
+
+def plot2(XS, binding_break_rates, concealing_break_rates, title="probability"):
+	plt.plot(XS, binding_break_rates, linewidth=2.0)
+	plt.plot(XS, concealing_break_rates, linewidth=2.0)
+	plt.xlabel('X, number of bits in hash outputs')
+	plt.ylabel('probability')
+	plt.legend(["probability of\nbreaking binding",
+		"probability of\nbreaking concealing"], loc='upper left')
+	plt.title(title)
+	plt.show()
 
 def main():
 	random.seed(1)
@@ -97,43 +96,19 @@ def main():
 	binding_break_rates = []
 	concealing_break_rates = []
 	for X in XS:
-		print "X =", X
 		v = (X & 0x1)
-		binding_break_rate, de_concealing_rate = simulate_break(X, v, n_itter)
+		binding_break_rate, concealing_break_rate = simulate_break(X, v, n_itter)
 		binding_break_rates.append(binding_break_rate)
-		concealing_break_rates.append(de_concealing_rate)
+		concealing_break_rates.append(concealing_break_rate)
 
-	print "displaying break binding success rate plot"
-	#plot(XS, binding_break_rates, title ="success rate",
-	#	X_label="bits", Y_label="success rate")
-
-	print "displaying break concealing success rate plot"
-	#plot(XS, concealing_break_rates, title ="de-success probability rate",
-	#	X_label="bits", Y_label="success rate")
-
-	plot2(XS, binding_break_rates, concealing_break_rates, title="break rates")
-
-def plot(X, Y, title="plot", X_label="X", Y_label="Y"):
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-	X = np.array(X)
-	Y = np.array(Y)
-	ax.bar(X, Y)
-	plt.title(title)
-	ax.set_xlabel(X_label)
-	ax.set_ylabel(Y_label)
-	plt.show()
-
-def plot2(XS, binding_break_rates, concealing_break_rates, title="break rates"):
-	plt.plot(XS, binding_break_rates)
-	plt.plot(XS, concealing_break_rates)
-	plt.xlabel('X, number of bits in hash outputs')
-	plt.ylabel('probability')
-	plt.legend(['binding_break_rates', 'concealing_break_rates'], loc='upper left')
-	plt.title(title)
-	plt.show()
-
+	plot1(XS, binding_break_rates, Y_label=
+		"probability of\nbreaking binding",
+		title="probability of breaking binding")
+	plot1(XS, concealing_break_rates, Y_label=
+		"probability of\nbreaking concealing",
+		title="probability of breaking concealing")
+	plot2(XS, binding_break_rates, concealing_break_rates,
+		title="probability rates")
 
 if __name__ == '__main__':
-	#print "hash(john) = ", Hash(16).hash_f("john2","NEJ")
 	main()

cf0504ff375c0b76ccbdf39ff840b81ec946d730
Johan Nyholm <hansjohannyholm@gmail.com>
2015-12-16 20:52:41 +0100

    Add ui for client

--- A5/C1/client.php
+++ A5/C1/client.php
@@ -1,7 +1,27 @@
 <?php
+if (!isset($_POST['submitok'])){
+?>
+	<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+		Name: <input type="text" name="name"><br>
+		Age: <input type="text" name="age"><br>
+		<input type="submit" name="submitok">
+	</form>
+<?php
+}else{
+	if ($_POST['name']=='' or $_POST['age']==''){
+        print('One or more required fields were left blank.\n'.'Please fill them in and try again.');
+    	return;
+    }
+    if (!is_numeric($_POST['age'])){
+    	print('Please make sure the age is an integer');
+    	return;
+    }
+    $name = $_POST['name'];
+    $age = intval($_POST['age']);
 	$wsdl = "http://localhost/A5/C1/pfService.wsdl";
 	$client = new SoapClient($wsdl);
-	$response = $client->__soapCall("getRandom", 
-		array("min" => 1,"max" => 100));
+	$response = $client->__soapCall("predictFuture", 
+		array("age" => $age,"name" => $name));
 	print($response);
+}
 ?>
--- A5/C1/service.php
+++ A5/C1/service.php
@@ -1,9 +1,40 @@
 <?php
-	function getRandom($min,$max){
+	function predictFuture($age,$name){
 		//code for RNG
-		return 10;
+		 
+		$futureEvents = "Nothing out of the ordinary will happen to you";
+		$futureAge = 100;
+		$futurePassion = "Undecided";
+		//predict the age
+		if ( $age < 0 ){
+			return "You are not yet born, sorry, no future can be predicted";
+		}else if( $age < 20 ){
+			$futureAge = $age + 80;
+		}else if( $age > 100 ){
+			$futureAge = $age + 1;
+		}
+
+		if ( $name == "Melinda" ){
+			$futureEvents = "You will learn to sky dive";
+		}else if( $name == "Steve" && $age < 80){
+			$futureEvents = "You will find a passion for spring onion";
+		}
+
+		$magicVal = (hash($name) + hash($age)) % 3;
+		if ( $magicVal == 0 ){
+			$futurePassion = "bird watching";
+		}else if( $magicVal == 1 ){
+			$futurePassion = "calendar collector";
+		}else if( $magicVal == 2 ){
+			$futurePassion = "snake charmer";
+		}
+
+		$ageInfo = "You will live for at least a total of {$futureAge} years";
+		$futurePassionInfo = "Your future passion will be {$futurePassion}";
+		return "{$ageInfo}</br>{$futureEvents}<br>{$futurePassionInfo}";
 	}
+
 	$server = new SoapServer("pfService.wsdl");
-	$server->addFunction("getRandom");
+	$server->addFunction("predictFuture");
 	$server->handle();
 ?>
